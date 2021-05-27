87a06b87590293401d80a34602c3f22aeae62c4a
Jordan Onwodi <>
2021-05-27 17:50:39 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# class Student:
+#     def __init__(self, scores = []):
+#         self.scores = scores
+    
+#     def avg(self):
+#         return round(sum(self.scores) / len(self.scores))
+
+#     @staticmethod
+#     def notice():
+#         return "The amount of students who passed their exams!"
+
+# Jordan = Student(scores = [3,4,5,6,7,2])
+
+# print(Student.notice())
+# print(Jordan.notice())
